2f9f82714219fb2cf865580250df89746140e49e
hajjamn <hajjamn@gmail.com>
2024-03-07 14:31:52 +0100

    ADD: valori dadi

--- Dadi/js/app.js
+++ Dadi/js/app.js
@@ -1,5 +1,9 @@
 //Genero il numero random per il giocatore e per il computer
 
+const playerNumber = Math.floor(Math.random() * (6) ) + 1;
+const houseNumber = Math.floor(Math.random() * (6) ) + 1;
+console.log(playerNumber, houseNumber);
+
 //Confronto i risultati ottenuti
   //Se il valore del giocatore e superiore a quello del pc stampo messaggio vittoria
   //Se il valore del giocatore e inferiore a quello del pc stampo messaggio di sconfitta
