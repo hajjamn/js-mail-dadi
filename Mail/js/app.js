d9c56f6872c1d7df585c189282c3cf52720ca017
hajjamn <hajjamn@gmail.com>
2024-03-07 13:56:33 +0100

    ADD: logica

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -1,1 +1,22 @@
 console.log('ciao mondo')
+
+// Chiedo la email all-utente con un prompt
+let userEmail = prompt('Inserisci la tua email')
+
+// Scrivo una lista di email accettate
+
+const verifiedEmails = ['donato.riccio@gmail.com', 'samuel.panicucci@gmail.com'];
+
+// Definisco una variabile per verificare se l email dell utente e verificata
+
+let isUserEmailVerified = false
+
+// Verifico se la mail inserita e tra quelle verificate
+
+  //controlla tutte le emailVerificate
+  //Se il valore di userEmail e uguale ad una di quelle nell array allora cambio il valore di isUserEmailVerified a true
+  
+  //Stamo il messaggio appropriato in console
+
+    //Se il valore di isUserEmailVerified e vero stampo messaggio di successo
+    //Altrimenti stampo messaggio di fallimento
