507e201ca28838327b8b5235387be96750467a9e
hajjamn <hajjamn@gmail.com>
2024-03-07 14:34:48 +0100

    ADD: aggiunti messaggi console

--- Dadi/js/app.js
+++ Dadi/js/app.js
@@ -6,5 +6,15 @@ console.log(playerNumber, houseNumber);
 
 //Confronto i risultati ottenuti
   //Se il valore del giocatore e superiore a quello del pc stampo messaggio vittoria
-  //Se il valore del giocatore e inferiore a quello del pc stampo messaggio di sconfitta
-  //Altrimenti stampo messaggio di pareggio
+  if (playerNumber > houseNumber) {
+    console.log('Hai vinto!')
+  } else if (
+    //Se il valore del giocatore e inferiore a quello del pc stampo messaggio di sconfitta
+    playerNumber < houseNumber ) {
+      console.log('Oh no, hai perso!')
+    } else {
+      //Altrimenti stampo messaggio di pareggio
+      console.log('Pareggio. Riprova!')
+    }
+    
+  
