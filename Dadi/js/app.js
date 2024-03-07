a783cf4c3a85d4068bd8a3fc38a78c6583cf39b3
hajjamn <hajjamn@gmail.com>
2024-03-07 14:27:22 +0100

    ADD: logica Dadi

--- Dadi/js/app.js
+++ Dadi/js/app.js
@@ -1,1 +1,6 @@
-console.log('ciao mondo')
+//Genero il numero random per il giocatore e per il computer
+
+//Confronto i risultati ottenuti
+  //Se il valore del giocatore e superiore a quello del pc stampo messaggio vittoria
+  //Se il valore del giocatore e inferiore a quello del pc stampo messaggio di sconfitta
+  //Altrimenti stampo messaggio di pareggio
